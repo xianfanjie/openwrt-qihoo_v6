42db1c543ba9180bcf20d7eb0a8df6fff289962a
xianfanjie <102248059+xianfanjie@users.noreply.github.com>
2024-12-04 03:03:51 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -18,12 +18,21 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
+# 修改WiFi名称
 sed -i 's/ssid=LEDE/ssid=$([ $devidx -eq 0 ] \&\& echo "OpenWrt_5G" || echo "OpenWrt_2.4G")/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改默认IP
 sed -i 's/192.168.1/192.168.0/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1/192.168.0/g' package/base-files/luci2/bin/config_generate
+# 修改默认主机名
 sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/OpenWrt/g' package/base-files/luci2/bin/config_generate
+# 修改默认时区 
+sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
+sed -i 's/UTC/CST-8/g' package/base-files/luci2/bin/config_generate
+# 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
+
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
