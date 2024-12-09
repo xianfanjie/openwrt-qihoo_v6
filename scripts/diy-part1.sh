ee41be04a1c70cbfed383dba4b3fbcc84ee22e53
xianfanjie <102248059+xianfanjie@users.noreply.github.com>
2024-12-09 19:29:49 +0800

    Update: 2024/12/09 周一

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -20,13 +20,26 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/l
 # Alist
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/alist
 
-git_sparse_clone master https://github.com/immortalwrt/immortalwrt package/emortal/automount
+git_sparse_clone master https://github.com/immortalwrt/immortalwrt package/emortal/automount package/emortal/cpufreq package/emortal/autocore
+git_sparse_clone master https://github.com/immortalwrt/luci applications/luci-app-vlmcsd applications/luci-app-cpufreq
+git_sparse_clone master https://github.com/immortalwrt/packages net/vlmcsd
 
-./scripts/feeds update -a && ./scripts/feeds install -a
+# git_sparse_clone master https://github.com/coolsnowwolf/luci applications/luci-app-vlmcsd
+# git_sparse_clone master https://github.com/coolsnowwolf/packages net/vlmcsd
+./scripts/feeds update -a
 
+# 修改默认主题
+sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+
+# 修改 Makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
 
 sed -i 's/PKG_VERSION:=1.37.0/PKG_VERSION:=1.36.0/' feeds/packages/net/aria2/Makefile
 sed -i 's/^PKG_HASH:=.*$/PKG_HASH:=58d1e7608c12404f0229a3d9a4953d0d00c18040504498b483305bcb3de907a5/' feeds/packages/net/aria2/Makefile
+
+./scripts/feeds install -a
 
 
--- scripts/openwrt-diy-part2.sh
+++ scripts/openwrt-diy-part2.sh
@@ -21,7 +21,5 @@
 
 # 修改默认IP
 sed -i 's/192.168.1/192.168.0/g' package/base-files/files/bin/config_generate
-# 修改默认时区
-#sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 # 修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+#sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
